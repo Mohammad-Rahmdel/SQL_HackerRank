2caa8aece35c53e1b24069191fb6622f867f2649
Mohammad-Rahmdel <unknown13766@yahoo.com>
2019-03-24 15:06:38 +0430

    Interviews.sql done.

--- Advanced Join/Interviews.sql
+++ Advanced Join/Interviews.sql
@@ -1,25 +1,13 @@
-select contest_id, hacker_id, name, sum(w1) z1, sum(w2) z2, sum(w3) z3, sum(w4) z4 
-from Contests natural join
-(select contest_id, sum(y1) w1, sum(y2) w2, sum(y3) w3, sum(y4) w4
-from Colleges natural join 
-(select college_id, sum(x1) y1, sum(x2) y2, sum(x3) y3, sum(x4) y4 
-from (select *
-    from (
-    select challenge_id, sum(total_submissions) as x1, sum(total_accepted_submissions) as x2
-    from Submission_Stats
-    group by challenge_id) sub1 
-    natural join (
-    select challenge_id, sum(total_views) as x3, sum(total_unique_views) as x4
-    from View_Stats
-    group by challenge_id) sub2
-      ) sub3
-natural join Challenges
-group by college_id) sub4
-group by contest_id, college_id
-) as sub5
-
-group by contest_id, hacker_id, name
-having z1<>0 or z2<>0 or z3<>0 or z4<>0  
-order by contest_id
-
-
+select t1.contest_id, t1.hacker_id, t1.name, sum(x3) y1, sum(x4) y2, sum(x1) y3, sum(x2) y4
+from contests as t1 
+inner join colleges as t2 on t1.contest_id = t2.contest_id 
+inner join challenges as t3 on  t2.college_id = t3.college_id 
+left join
+(select challenge_id, sum(total_views) as x1, sum(total_unique_views) as x2
+from view_stats group by challenge_id) sub1 on t3.challenge_id = sub1.challenge_id 
+left join
+(select challenge_id, sum(total_submissions) as x3, sum(total_accepted_submissions) as x4
+ from submission_stats group by challenge_id) sub3 on t3.challenge_id = sub3.challenge_id
+    group by t1.contest_id, t1.hacker_id, t1.name
+        having (y1+y2+y3+y4) <> 0
+            order by t1.contest_id;
